e1ce8893a7f6f7387c6c7bf0c60a4c0793d46fb7
huui <gmlwl0408@gmail.com>
2023-01-21 16:21:53 +0900

    snail Test

---
+++ ChoiHuiji/snail/snail/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace snail;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int y = 1;
+
+        while(y < 5)
+        {          
+            for (int x = 1; x < 50; ++x)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+
+                if (x % 3 == 0)
+                {
+                    Console.Write("__@");
+                }
+
+                else if (x % 3 == 1)
+                {
+                    Console.Write("_^@");
+                }
+
+                else
+                {
+                    Console.Write("^_@");
+                }
+                Thread.Sleep(100);
+              
+            }
+        }
+        
+        
+    }
+}
+
