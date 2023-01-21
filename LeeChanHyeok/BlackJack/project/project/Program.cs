c883849733c5caf527ce1ede01cc6c2950637a86
lees5402 <lees5402@naver.com>
2023-01-22 00:33:14 +0900

    새로운 커밋

---
+++ LeeChanHyeok/BlackJack/project/project/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Title titleSceen = new Title();
+            //titleSceen.TitleMain();
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.Title = "블랙잭";
+            Console.CursorVisible= false;
+            
+
+            //맵 함수 부분
+            void MapExtent(int x, int y, string mapObject)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(mapObject);
+            }
+            
+            while (true)
+            {
+                //------------------------------------랜더-------------------------------------
+                Console.Clear();
+                //블랙잭 맵
+                for(int i = 0; i < 125; ++i)
+                {
+                    MapExtent(30 + i, 3, "#");
+                    MapExtent(30 + i, 37, "#");
+                    
+                }
+                for(int i = 0; i < 35; ++i)
+                {
+                    MapExtent(30, 3 + i, "#");
+                    MapExtent(155, 3 + i, "#");
+
+                }
+                
+                //명령어 맵
+                for(int i = 0; i < 35; ++i)
+                {
+                    MapExtent(119 + i, 5, "-");
+                    MapExtent(119 + i, 7, "-");
+                }
+
+                for(int i = 0; i < 3; ++i)
+                {
+                    MapExtent(119, 5 + i, "l");
+                    MapExtent(153, 5 + i, "l");
+                }
+
+                // 소지금
+                MapExtent(120, 6, "소지금 : {}");
+
+                //메뉴 테두리
+                for(int i = 0; i < 35; ++i)
+                {
+                    MapExtent(119 + i, 10, "#");
+                    MapExtent(119 + i, 20, "#");
+                }
+                for(int i = 0; i < 10; ++i)
+                {
+                    MapExtent(119, 10 + i, "#");
+                    MapExtent(153, 10 + i, "#");
+                }
+
+                //메뉴
+                MapExtent(126, 12, "▶ " + "Raise");
+                MapExtent(126, 15, "▶ " + "Check");
+                MapExtent(126, 18, "▶ " + "Fold");
+
+                //------------------------------------ProcessInput ----------------------------
+                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                ConsoleKey key = keyinfo.Key;
+
+                //------------------------------------update-----------------------------------
+                
+            }
+        }
+    }
+}
---
+++ LeeChanHyeok/BlackJack/project/project/Title.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Title
+    {
+        public void TitleMain()
+        {
+            TitleSceen();
+        }
+        private void TitleSceen()
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            const int titleShapeX = 60;
+
+            while (true)
+            {
+                Console.CursorVisible = false;
+                for (int titleId = 0; titleId < titleShapeX; ++titleId)
+                {
+                    TitleRender(titleShapeX + titleId, 3, "#");
+                    TitleRender(titleShapeX + titleId, 20, "#");
+                    TitleRender(titleShapeX, 8, "______  _               _       ___               _    ");
+                    TitleRender(titleShapeX, 9, "| ___ \\| |             | |     |_  |             | |   ");
+                    TitleRender(titleShapeX, 10, "| |_/ /| |  __ _   ___ | | __    | |  __ _   ___ | | __");
+                    TitleRender(titleShapeX, 11, "| ___ \\| | / _` | / __|| |/ /    | | / _` | / __|| |/ /");
+                    TitleRender(titleShapeX, 12, "| |_/ /| || (_| || (__ |   < /\\__/ /| (_| || (__ |   < ");
+                    TitleRender(titleShapeX, 13, "\\____/ |_| \\__,_| \\___||_|\\_\\\\____/  \\__,_| \\___||_|\\_\\");
+                }
+
+                TitleRender(titleShapeX, 30, "▶ 게임을 시작하려면 Yes를 입력 : ");
+                string gameStart = Console.ReadLine();
+                switch (gameStart)
+                {
+                    case "yes":
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.Write("게임을 종료합니다.");
+                        Environment.Exit(0);
+                        break;
+                }
+
+            }
+
+
+
+        }
+
+        private void TitleRender(int x, int y, string titleObject)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(titleObject);
+        }
+    }
+}
